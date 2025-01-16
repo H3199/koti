c8feb166dcba6fb1e5c2f8888135a10a2a8125eb
H3199 <eerohaavisto@gmail.com>
2025-01-16 22:28:45 +0200

    Added murmur docker script and Jenkinsfile.

---
+++ murmur/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+MURMURDIR=~/murmur/data
+
+if [ -z "$MUMBLE_PASSWORD" ]; then
+    echo "Error: MUMBLE_PASSWORD is not set. Exiting."
+    exit 1
+fi
+
+if [ ! -d "$MURMURDIR" ]; then
+    echo "Directory $MURMURDIR does not exist. Creating it..."
+    mkdir -p "$MURMURDIR"
+else
+    echo "Directory $MURMURDIR already exists."
+fi
+
+docker run --detach \
+           --name mumble-server \
+           --publish 64738:64738/tcp \
+           --publish 64738:64738/udp \
+           --volume "$MURMURDIR:/data" \
+	   -e "MUMBLE_CONFIG_SERVER_PASSWORD=${MUMBLE_PASSWORD}" \
+           --restart on-failure \
+           mumblevoip/mumble-server:latest
